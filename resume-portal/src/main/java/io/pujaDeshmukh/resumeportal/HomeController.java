58953a0d99d025ca8898db6956a926b666434aa2
pujadeshmukh <pujatdeshmukh@gmail.com>
2022-09-18 18:11:52 +0530

    Added resume templates

--- resume-portal/src/main/java/io/pujaDeshmukh/resumeportal/HomeController.java
+++ resume-portal/src/main/java/io/pujaDeshmukh/resumeportal/HomeController.java
@@ -7,13 +7,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(){
-        return "Hello";
+    public String home(HttpServletRequest req){
+        HttpSession session = req.getSession();
+        String name = req.getParameter("name");
+        session.setAttribute("name",name);
+        return "index";
     }
     @GetMapping("/edit")
     public String edit(){
@@ -21,8 +27,8 @@ public class HomeController {
     }
 
     @GetMapping("/view/{userId}")
-    public String view(@PathVariable String userId, Model model){
-        model.addAttribute("userId",userId);
-        return "profile";
+    public String userView(@PathVariable String userId, Model model){
+       model.addAttribute("userId",userId);
+        return "profile-templates/2/index";
     }
 }
